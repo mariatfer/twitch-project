66317171ce5c9491309d4b0f62976db95307a295
María Fernández <matefe1706@gmail.com>
2025-04-03 14:19:32 +0200

    feat(components): aside component added

---
+++ components/TheAside.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import CollapseIcon from '@/components/icons/CollapseIcon.vue';
+</script>
+
+<template>
+  <aside class="aside">
+    <div class="aside__header">
+      <h2>Recommended channels</h2>
+      <TheButton><CollapseIcon/></TheButton>
+    </div>
+    <div class="aside__content">
+      <slot/>
+    </div>
+  </aside>
+</template>
+
+<style lang="scss" scoped>
+.aside {
+    display: flex;
+    flex-direction: column;
+    width: 16.25rem;
+}
+
+</style>
