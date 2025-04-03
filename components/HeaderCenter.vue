7b3cfd713bc526c91f3e08ce2564d80524dd1aeb
María Fernández <matefe1706@gmail.com>
2025-04-03 12:03:33 +0200

    feat(components): adding header components)

---
+++ components/HeaderCenter.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import SearchIcon from '@/components/icons/SearchIcon.vue'
+
+const searchButton = {
+  background: 'rgba(83, 83, 95, 0.38)',
+}
+</script>
+
+<template>
+  <section class="search">
+    <input
+      id="search"
+      type="search"
+      name="search"
+      class="search__input"
+      placeholder="Search"
+    >
+    <TheButton :style="searchButton" class="search__button"
+      ><SearchIcon
+    /></TheButton>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+@use '/assets/styles/mixins.scss' as *;
+
+.search {
+  @include flex(row, flex-start, center, wrap, 0);
+
+  &__input {
+    border: 0.0625rem solid rgb(255 255 255 / 70%);
+    border-top-left-radius: 0.3125rem;
+    border-bottom-left-radius: 0.3125rem;
+    padding: 0.65rem;
+    width: 24.375rem;
+    background: transparent;
+    transition: all 0.2s ease-in-out;
+    &:focus {
+      outline: none;
+      border-color: #189afc;
+    }
+    &:hover {
+      border-color: #189afc;
+    }
+    &::placeholder {
+      transition: all 0.2s ease-in-out;
+    }
+    &:hover::placeholder {
+      color: #dbdbdb;
+    }
+  }
+  &__button {
+    border: 0.0625rem solid rgb(83 83 95 / 38%);
+    transition: all 0.2s ease-in-out;
+    &:hover {
+      border: 0.0625rem solid #189afc;
+    }
+  }
+}
+</style>
---
+++ components/HeaderLeft.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import MoreIcon from '@/components/icons/MoreIcon.vue'
+import TheButton from './TheButton.vue'
+
+const iconButton = {
+  background: 'transparent',
+}
+</script>
+
+<template>
+  <section class="home">
+    <NuxtLink to="/" class="home__title">
+      <img src="/assets/midutch-logo.svg" alt="Midutch logo" width="28" height="28" >
+      <h1>MIDUTCH</h1>
+    </NuxtLink>
+    <TheButton :style="iconButton" class="home__button--scale"><MoreIcon /></TheButton>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+@use '/assets/styles/mixins.scss' as *;
+
+.home {
+  @include flex(row, flex-start, center, wrap, 0.5em);
+  &__title {
+    @include flex(row, space-between, center, wrap, 0.5em);
+
+    display: flex;
+    align-items: center;
+    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+    font-size: 0.8125rem;
+    transition: all 0.2s ease-in-out;
+    &:hover {
+      transform: translateY(-0.1rem);
+    }
+  }
+  &__button--scale:hover {
+    transform: scale(1.07);
+    transition: all 0.2s ease-in-out;
+  }
+}
+</style>
