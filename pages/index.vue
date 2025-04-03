f2df33e028157f0a8cb2e2525faddcd97be8f01d
María Fernández <matefe1706@gmail.com>
2025-04-03 17:43:36 +0200

    feat(pages): aside components added

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,29 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-    <div>
-        <TheHeader />
-        <main>
-            <TheAside/>
-        </main>
-    </div>
+  <div class="container">
+    <TheHeader />
+    <main class="container__main">
+      <TheAside />
+      <div class="main__content">
+        <LiveRecommendation />
+      </div>
+    </main>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+@use '/assets/styles/mixins.scss' as *;
+.container {
+  @include flex(column);
+  width: 100vw;
+
+  &__main {
+    @include flex(row, flex-start, flex-start, wrap, 0.625rem);
+  }
+}
+.main__content {
+  @include flex(column, flex-start, flex-start, wrap, 1.25rem);
+  width: calc(100% - 16.8738rem);
+}
+</style>
