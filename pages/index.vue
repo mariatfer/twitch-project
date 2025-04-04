cdf0d4e6940ff0657538d56508ab6341a9362d5e
María Fernández <matefe1706@gmail.com>
2025-04-04 13:22:03 +0200

    feat(pages): adding functionalities for retrieving user data and live streamings

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,59 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { TwitchAPI } from '@/utils/TwitchAPI'
+import getToken from '@/utils/TwitchAuth'
 
+const twitchApi = new TwitchAPI()
+const authUrl = ref<string>('')
+
+onMounted(async () => {
+  try {
+    authUrl.value = await twitchApi.getAuthorizationUrl()
+  } catch (error) {
+    console.error('Error obtaining Twitch authorization URL:', error)
+  }
+})
+
+async function handleTwitchCallback() {
+  const queryParams = new URLSearchParams(window.location.search)
+  const code = queryParams.get('code')
+
+  if (code) {
+    try {
+      const tokenResponse = await twitchApi.exchangeCodeForToken(code)
+      console.log('Access Token:', tokenResponse)
+
+      const users = await twitchApi.getUserData(tokenResponse.access_token)
+      console.log('Users:', users)
+    } catch (error) {
+      console.error('Error handling Twitch callback:', error)
+    }
+  } else {
+    console.error('The authorization code was not found in the URL.')
+  }
+}
+
+async function fetchLiveStreams() {
+  try {
+    const accessToken = await getToken()
+    const streams = await twitchApi.getLiveStreams(accessToken.access_token, 12)
+    console.log('Streamings:', streams)
+  } catch (error) {
+    console.error('Error obtaining live streams:', error)
+  }
+}
+
+onMounted(() => {
+  if (window.location.search.includes('code=')) {
+    handleTwitchCallback()
+    fetchLiveStreams()
+  }
+})
 </script>
 
 <template>
-    <div>
-        <TheHeader />
-        <h2>home</h2>
-    </div>
+  <div>
+    <TheHeader />
+    <h2>home</h2>
+  </div>
 </template>
-
-<style lang="scss" scoped>
-
-</style>
-    
