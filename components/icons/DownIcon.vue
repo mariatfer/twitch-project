2925d1627d55cc361099471a0b5887243dd37e76
María Fernández <matefe1706@gmail.com>
2025-04-06 20:08:30 +0200

    feat(components): added down icon

---
+++ components/icons/DownIcon.vue
@@ -0,0 +1,14 @@
+<template>
+  <svg
+    width="17"
+    height="17"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M3 8.5L5.22727 6.5L12.5 13.5L19.7727 6.5L22 8.5L12.5 17.5L3 8.5Z"
+      fill="#189AFC"
+    />
+  </svg>
+</template>
