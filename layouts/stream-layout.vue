35112c35890a30d2163ade094b8c655b6b313553
María Fernández <matefe1706@gmail.com>
2025-04-07 21:05:49 +0200

    feat(layout): layout for streaming page added

---
+++ layouts/stream-layout.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { TwitchAPI } from '@/utils/TwitchAPI'
+
+const twitchApi = new TwitchAPI()
+
+async function handleTwitchCallback() {
+  const queryParams = new URLSearchParams(window.location.search)
+  const code = queryParams.get('code')
+
+  if (code) {
+    try {
+      const tokenResponse = await twitchApi.exchangeCodeForToken(code)
+      const users = await twitchApi.getUserData(tokenResponse.access_token)
+      console.log('Users:', users)
+    } catch (error) {
+      console.error('Error handling Twitch callback:', error)
+    }
+  } else {
+    console.error('The authorization code was not found in the URL.')
+  }
+}
+
+onMounted(() => {
+  if (window.location.search.includes('code=')) {
+    handleTwitchCallback()
+  }
+})
+</script>
+
+<template>
+  <div class="container">
+    <header class="container__header">
+      <HeaderLeft />
+      <HeaderCenter />
+      <HeaderRight />
+    </header>
+    <main class="container__main">
+      <TheAside />
+      <slot />
+    </main>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  @include flex(column);
+  &__header {
+    height: 3.125rem;
+    background-color: #18181b;
+    padding: 0 1.25rem;
+    @include flex(row, space-between, center);
+  }
+  &__main {
+    @include flex(row, flex-start, flex-start, nowrap, 0.625rem);
+    width: 100%;
+    padding-left: 1.25rem;
+  }
+}
+</style>
