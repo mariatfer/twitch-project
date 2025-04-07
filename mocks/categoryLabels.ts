fd05bd02553dd807e096581bc359d94a9c0ac217
María Fernández <matefe1706@gmail.com>
2025-04-07 15:08:29 +0200

    feat(mocks): added mocks for category labels

---
+++ mocks/categoryLabels.ts
@@ -0,0 +1,13 @@
+import GamingIcon from '@/components/icons/GamingIcon.vue';
+import CreativeIcon from '@/components/icons/CreativeIcon.vue';
+import EsportsIcon from '@/components/icons/EsportsIcon.vue';
+import MusicIcon from '@/components/icons/MusicIcon.vue';
+import IRLIcon from '@/components/icons/IRLIcon.vue';
+
+export const categoryLabels = [
+    { id: '1', text: 'Games', icon: GamingIcon },
+    { id: '2', text: 'IRL', icon: IRLIcon },
+    { id: '3', text: 'Music & DJs', icon: MusicIcon },
+    { id: '4', text: 'Creative', icon: CreativeIcon },
+    { id: '5', text: 'Esports', icon: EsportsIcon },
+  ];
